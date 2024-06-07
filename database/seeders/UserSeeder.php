cae4a10c4bb752f45784ada5e8fa29a7467eaa64
bagusuntoro <untorobagus65@gmail.com>
2024-06-07 13:25:28 +0700

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Project;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -22,6 +23,20 @@ class UserSeeder extends Seeder
             'password' => Hash::make('12345'),
             'level' => '0',
             'noHP' => '0812345'
+        ]);
+
+        Project::create([
+            'client' => 'satu',
+            'project' => 'satu',
+            'schedule' => 'satu',
+            'dueDate' => '2024/05/01',
+            'status' => '1'
+        ]);
+        Project::create([
+            'client' => 'dua',
+            'project' => 'dua',
+            'schedule' => 'dua',
+            'dueDate' => '2024/06/01',
         ]);
     }
 }
